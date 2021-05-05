6023ec5f85ba7ddb5b93dbdd4ac035bffdc4dcf2
Leonardo Delfino <leonardossdelfino@gmail.com>
2021-05-04 23:05:58 -0300

    Aula de JS, variáveis e escopo.

---
+++ modulo-javascript/scope.js
@@ -0,0 +1,12 @@
+// global
+{
+    var num1 = 1
+}
+
+console.log(num1)
+
+// local
+{
+    let x = 0
+    console.log(x)
+}
---
+++ modulo-javascript/variaveis.js
@@ -0,0 +1,12 @@
+// var (é global)
+var clima = "Quente"
+// Reatribuição de valor
+clima = "Frio"
+// let (local)
+let clima2 = "Frio"
+// const (Local), não muda o tipo do valor
+const clima3 = "Quente"
+
+console.log(clima)
+console.log(clima2)
+console.log(clima3)
